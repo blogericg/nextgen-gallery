683abcb42b93ff0ac225b6deed3062b554640bca
Benjamin Owens <benjamin@imagely.com>
2020-07-21 13:36:45 -0700

    Fixing Lightbox settings panel being hidden when Pro is active

--- products/photocrati_nextgen/modules/nextgen_other_options/templates/lightbox_library_tab.php
+++ products/photocrati_nextgen/modules/nextgen_other_options/templates/lightbox_library_tab.php
@@ -30,19 +30,21 @@
         </td>
     </tr>
 
-	<tr>
-		<td class="column1">
-			<label for="lightbox_global"><?php esc_html_e('What must the lightbox be applied to?', 'nggallery')?></label>
-		</td>
-		<td>
-			<select name="thumbEffectContext" id="lightbox_global">
-                <option value="nextgen_images" <?php selected('nextgen_images', $lightbox_global, TRUE)?>><?php esc_html_e('Only apply to NextGEN images', 'nggallery'); ?></option>
-                <option value="nextgen_and_wp_images" <?php selected('nextgen_and_wp_images', $lightbox_global, TRUE)?>><?php esc_html_e('Only apply to NextGEN and WordPress images', 'nggallery'); ?></option>
-                <option value="all_images" <?php selected('all_images', $lightbox_global, TRUE)?>><?php esc_html_e('Try to apply to all images', 'nggallery'); ?></option>
-                <option value="all_images_direct" <?php selected('all_images_direct', $lightbox_global, TRUE)?>><?php esc_html_e('Try to apply to all images that link to image files', 'nggallery'); ?></option>
-            </select>
-        </td>
-    </tr>
+    <tbody>
+        <tr>
+            <td class="column1">
+                <label for="lightbox_global"><?php esc_html_e('What must the lightbox be applied to?', 'nggallery')?></label>
+            </td>
+            <td>
+                <select name="thumbEffectContext" id="lightbox_global">
+                    <option value="nextgen_images" <?php selected('nextgen_images', $lightbox_global, TRUE)?>><?php esc_html_e('Only apply to NextGEN images', 'nggallery'); ?></option>
+                    <option value="nextgen_and_wp_images" <?php selected('nextgen_and_wp_images', $lightbox_global, TRUE)?>><?php esc_html_e('Only apply to NextGEN and WordPress images', 'nggallery'); ?></option>
+                    <option value="all_images" <?php selected('all_images', $lightbox_global, TRUE)?>><?php esc_html_e('Try to apply to all images', 'nggallery'); ?></option>
+                    <option value="all_images_direct" <?php selected('all_images_direct', $lightbox_global, TRUE)?>><?php esc_html_e('Try to apply to all images that link to image files', 'nggallery'); ?></option>
+                </select>
+            </td>
+        </tr>
+    </tbody>
 
     <?php foreach ($sub_fields as $name => $form) { ?>
         <tbody class="lightbox_library_settings hidden" id="lightbox_library_<?php echo esc_attr($name); ?>">
