b4588d3ea7beb6a00293516ffa4d530ec45cfe76
Benjamin Owens <benjamin@imagely.com>
2020-08-20 14:03:09 -0700

    Removing extra console.log() left over

--- products/photocrati_nextgen/modules/attach_to_post/static/display_tab.js
+++ products/photocrati_nextgen/modules/attach_to_post/static/display_tab.js
@@ -335,8 +335,6 @@ jQuery(function($){
             obj.display_type = display_type.get_shortcode_value();
 
 
-            console.log("Rendering shortcode", obj);
-
             // Convert the displayed gallery to a shortcode
             var snippet = '[ngg';
             var val = null;
