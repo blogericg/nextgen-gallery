28be9a03bdd207748d2a2d3d8b461924589097ff
Michael Weichert <mweichert@gmail.com>
2021-01-18 08:32:43 -0500

    Fixed fancybox with jquery-mmenu

--- products/photocrati_nextgen/modules/lightbox/static/fancybox/nextgen_fancybox_init.js
+++ products/photocrati_nextgen/modules/lightbox/static/fancybox/nextgen_fancybox_init.js
@@ -1,14 +1,27 @@
 jQuery(function($) {
     var nextgen_fancybox_init = function() {
-    		var selector = nextgen_lightbox_filter_selector($, $(".ngg-fancybox"));
-    		
-        selector.fancybox({
-            titlePosition: 'inside',
-            // Needed for twenty eleven
-            onComplete: function() {
-                $('#fancybox-wrap').css('z-index', 10000);
-            }
-        });
+        var selector = nextgen_lightbox_filter_selector($, $(".ngg-fancybox"));
+
+        window.addEventListener(
+            "click",
+            e => {
+                let $target = $(e.target);
+                if ($target.is(selector) || $target.parents('a').is(selector)) {
+                    $link = e.target.nodeName == "IMG" ? $target : $target.find('img')
+                    e.preventDefault()
+                    $.fancybox($link[0].outerHTML, {
+                        titlePosition: 'inside',
+                        // Needed for twenty eleven
+                        onComplete: function() {
+                            $('#fancybox-wrap').css('z-index', 10000);
+                        }
+                    })
+                    
+                    e.stopPropagation();
+                }
+            },
+            true
+        )
     };
     $(window).on('refreshed', nextgen_fancybox_init);
     nextgen_fancybox_init();
